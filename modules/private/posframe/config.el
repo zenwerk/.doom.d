dd02f7e43de8f52cb5e9866dc76901dee1f902e0
zenwerk <zenwerk@localhost>
2019-05-16 15:27:45 +0900

    update ivy-posframe params

--- modules/private/posframe/config.el
+++ modules/private/posframe/config.el
@@ -8,12 +8,23 @@
     :g "C-S-o" #'counsel-find-file))
 
 (def-package! ivy-posframe
+  :custom
+  (ivy-posframe-parameters
+   '((left-fringe . 5)
+     (right-fringe . 5)))
+  (ivy-posframe-border-width 7)
+  :custom-face
+  (ivy-posframe ((t (:background "#282a36"))))
+  ;; (ivy-posframe-border ((t (:background "#6272a4"))))
+  ;; (ivy-posframe-cursor ((t (:background "#61bfff"))))
   :config
   (setq ivy-display-function #'ivy-posframe-display-at-frame-center)
-  (ivy-posframe-enable))
+  (ivy-posframe-enable)
+  )
 
 (def-package! hydra-posframe
-  :hook (after-init . hydra-posframe-enable))
+  :config
+  (hydra-posframe-enable))
 
 (def-package! flymake-posframe
   :hook (flymake-mode . flymake-posframe-mode))
